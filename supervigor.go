9649a8fbcba5c397f436f4b740ab6e1a3eac6eb7
mrkaspa <michel.ingesoft@gmail.com>
2016-08-23 15:39:58 -0500

    formating code

--- supervigor.go
+++ supervigor.go
@@ -1,97 +1,96 @@
 package supervigor
 
 import (
-	"fmt"
-	"sync"
+  "fmt"
+  "sync"
 )
 
 // A Supervigor starts and supervises goroutines
 type Supervigor struct {
-	superviseChan chan RunnableWithName
-	runnables     map[string]*runnableWithChan
-	mapMutex      *sync.Mutex
+  superviseChan chan RunnableWithName
+  runnables     map[string]*runnableWithChan
+  mapMutex      *sync.Mutex
 }
 
 type runnableWithChan struct {
-	rchan    chan bool
-	restarts int
-	runnable Runnable
+  rchan    chan bool
+  restarts int
+  runnable Runnable
 }
 
 // A RunnableWithName packs the name of the goroutine,
 // the max amount of restarts and the Runnble object to run
 type RunnableWithName struct {
-	Name        string
-	MaxRestarts int
-	Runnable    Runnable
+  Name        string
+  MaxRestarts int
+  Runnable    Runnable
 }
 
-// Read is the basic interface that wraps the Run method.
-// Run() is the main thread for the goroutine that will be
+// Runnable is the main thread for the goroutine that will be
 // started and restarted
 type Runnable interface {
-	Run()
+  Run()
 }
 
 // NewSupervigor returns and runs in a goroutine the Supervigor
 func NewSupervigor() Supervigor {
-	s := Supervigor{
-		superviseChan: make(chan RunnableWithName),
-		runnables:     map[string]*runnableWithChan{},
-		mapMutex:      &sync.Mutex{},
-	}
-	go s.run()
-	return s
+  s := Supervigor{
+    superviseChan: make(chan RunnableWithName),
+    runnables:     map[string]*runnableWithChan{},
+    mapMutex:      &sync.Mutex{},
+  }
+  go s.run()
+  return s
 }
 
 // Supervise a runnable
 func (s *Supervigor) Supervise(rwn RunnableWithName) {
-	s.superviseChan <- rwn
+  s.superviseChan <- rwn
 }
 
 func (s *Supervigor) run() {
-	for {
-		select {
-		case rwn := <-s.superviseChan:
-			fmt.Printf("supervising %s \n", rwn.Name)
-			s.runAndSupervise(rwn.Name, rwn.MaxRestarts, rwn.Runnable)
-		}
-	}
+  for {
+    select {
+    case rwn := <-s.superviseChan:
+      fmt.Printf("supervising %s \n", rwn.Name)
+      s.runAndSupervise(rwn.Name, rwn.MaxRestarts, rwn.Runnable)
+    }
+  }
 }
 
 func (s *Supervigor) runAndSupervise(name string, maxRestarts int, r Runnable) {
-	s.mapMutex.Lock()
-	rwc, ok := s.runnables[name]
-	if !ok {
-		rwc = &runnableWithChan{
-			rchan:    make(chan bool),
-			runnable: r,
-		}
-		s.runnables[name] = rwc
-	}
-	s.mapMutex.Unlock()
+  s.mapMutex.Lock()
+  rwc, ok := s.runnables[name]
+  if !ok {
+    rwc = &runnableWithChan{
+      rchan:    make(chan bool),
+      runnable: r,
+    }
+    s.runnables[name] = rwc
+  }
+  s.mapMutex.Unlock()
 
-	//supervise the goroutine
-	go func() {
-		<-rwc.rchan
-		s.mapMutex.Lock()
-		s.runnables[name].restarts++
-		if s.runnables[name].restarts <= maxRestarts {
-			fmt.Printf("restarting #%d %s \n", s.runnables[name].restarts, name)
-			s.superviseChan <- RunnableWithName{name, maxRestarts, r}
-		}
-		s.mapMutex.Unlock()
-	}()
+  //supervise the goroutine
+  go func() {
+    <-rwc.rchan
+    s.mapMutex.Lock()
+    s.runnables[name].restarts++
+    if s.runnables[name].restarts <= maxRestarts {
+      fmt.Printf("restarting #%d %s \n", s.runnables[name].restarts, name)
+      s.superviseChan <- RunnableWithName{name, maxRestarts, r}
+    }
+    s.mapMutex.Unlock()
+  }()
 
-	go run(rwc)
+  go run(rwc)
 }
 
 // run the go routine if it panics notify to the supervigor
 func run(rwc *runnableWithChan) {
-	defer func() {
-		if r := recover(); r != nil {
-			rwc.rchan <- true
-		}
-	}()
-	rwc.runnable.Run()
+  defer func() {
+    if r := recover(); r != nil {
+      rwc.rchan <- true
+    }
+  }()
+  rwc.runnable.Run()
 }
--- supervigor_test.go
+++ supervigor_test.go
@@ -1,53 +1,53 @@
 package supervigor_test
 
 import (
-	"fmt"
-	"github.com/mrkaspa/supervigor"
-	"github.com/stretchr/testify/assert"
-	"testing"
-	"time"
+  "fmt"
+  "github.com/mrkaspa/supervigor"
+  "github.com/stretchr/testify/assert"
+  "testing"
+  "time"
 )
 
 type Runner struct {
-	name      string
-	mustPanic bool
-	doneChan  chan bool
+  name      string
+  mustPanic bool
+  doneChan  chan bool
 }
 
 func (r *Runner) Run() {
-	if r.mustPanic {
-		r.doneChan <- false
-		r.mustPanic = false
-		panic("")
-	}
-	fmt.Printf("Hi I'm %s\n", r.name)
-	r.doneChan <- true
+  if r.mustPanic {
+    r.doneChan <- false
+    r.mustPanic = false
+    panic("")
+  }
+  fmt.Printf("Hi I'm %s\n", r.name)
+  r.doneChan <- true
 }
 
 func TestNewSupervigor(t *testing.T) {
-	sup := supervigor.NewSupervigor()
-	run := &Runner{"Michel", true, make(chan bool)}
-	sup.Supervise(supervigor.RunnableWithName{
-		Name:        "demo",
-		MaxRestarts: 1,
-		Runnable:    run,
-	})
-	assert.False(t, <-run.doneChan)
-	assert.True(t, <-run.doneChan)
+  sup := supervigor.NewSupervigor()
+  run := &Runner{"Michel", true, make(chan bool)}
+  sup.Supervise(supervigor.RunnableWithName{
+    Name:        "demo",
+    MaxRestarts: 1,
+    Runnable:    run,
+  })
+  assert.False(t, <-run.doneChan)
+  assert.True(t, <-run.doneChan)
 }
 
 func TestNewSupervigorMustFail(t *testing.T) {
-	sup := supervigor.NewSupervigor()
-	run := &Runner{"Michel", true, make(chan bool)}
-	sup.Supervise(supervigor.RunnableWithName{
-		Name:        "demo",
-		MaxRestarts: 0,
-		Runnable:    run,
-	})
-	assert.False(t, <-run.doneChan)
-	select {
-	case <-run.doneChan:
-		t.Error("Should not enter here")
-	case <-time.NewTimer(1 * time.Second).C:
-	}
+  sup := supervigor.NewSupervigor()
+  run := &Runner{"Michel", true, make(chan bool)}
+  sup.Supervise(supervigor.RunnableWithName{
+    Name:        "demo",
+    MaxRestarts: 0,
+    Runnable:    run,
+  })
+  assert.False(t, <-run.doneChan)
+  select {
+  case <-run.doneChan:
+    t.Error("Should not enter here")
+  case <-time.NewTimer(1 * time.Second).C:
+  }
 }
