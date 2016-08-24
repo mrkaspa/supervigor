61708b6ac9f3dfd11c5032e5504478cad154f1f8
mrkaspa <michel.ingesoft@gmail.com>
2016-08-24 16:46:32 -0500

    tests updated

--- supervigor_test.go
+++ supervigor_test.go
@@ -1,53 +1,58 @@
 package supervigor_test
 
 import (
-  "fmt"
-  "github.com/mrkaspa/supervigor"
-  "github.com/stretchr/testify/assert"
-  "testing"
-  "time"
+	"fmt"
+	"github.com/mrkaspa/supervigor"
+	"github.com/stretchr/testify/assert"
+	"testing"
+	"time"
 )
 
 type Runner struct {
-  name      string
-  mustPanic bool
-  doneChan  chan bool
+	name      string
+	mustPanic bool
+	doneChan  chan bool
 }
 
 func (r *Runner) Run() {
-  if r.mustPanic {
-    r.doneChan <- false
-    r.mustPanic = false
-    panic("")
-  }
-  fmt.Printf("Hi I'm %s\n", r.name)
-  r.doneChan <- true
+	if r.mustPanic {
+		r.doneChan <- false
+		r.mustPanic = false
+		panic("")
+	}
+	fmt.Printf("Hi I'm %s\n", r.name)
+	r.doneChan <- true
 }
 
 func TestNewSupervigor(t *testing.T) {
-  sup := supervigor.NewSupervigor()
-  run := &Runner{"Michel", true, make(chan bool)}
-  sup.Supervise(supervigor.RunnableWithName{
-    Name:        "demo",
-    MaxRestarts: 1,
-    Runnable:    run,
-  })
-  assert.False(t, <-run.doneChan)
-  assert.True(t, <-run.doneChan)
+	t.Run("HappyPath", whenIsRestarted)
+	t.Run("UnhappyPath", whenIsNotRestarted)
 }
 
-func TestNewSupervigorMustFail(t *testing.T) {
-  sup := supervigor.NewSupervigor()
-  run := &Runner{"Michel", true, make(chan bool)}
-  sup.Supervise(supervigor.RunnableWithName{
-    Name:        "demo",
-    MaxRestarts: 0,
-    Runnable:    run,
-  })
-  assert.False(t, <-run.doneChan)
-  select {
-  case <-run.doneChan:
-    t.Error("Should not enter here")
-  case <-time.NewTimer(1 * time.Second).C:
-  }
+func whenIsRestarted(t *testing.T) {
+	sup := supervigor.NewSupervigor()
+	run := &Runner{"Michel", true, make(chan bool)}
+	sup.Supervise(supervigor.RunnableWithName{
+		Name:        "demo",
+		MaxRestarts: 1,
+		Runnable:    run,
+	})
+	assert.False(t, <-run.doneChan)
+	assert.True(t, <-run.doneChan)
+}
+
+func whenIsNotRestarted(t *testing.T) {
+	sup := supervigor.NewSupervigor()
+	run := &Runner{"Michel", true, make(chan bool)}
+	sup.Supervise(supervigor.RunnableWithName{
+		Name:        "demo",
+		MaxRestarts: 0,
+		Runnable:    run,
+	})
+	assert.False(t, <-run.doneChan)
+	select {
+	case <-run.doneChan:
+		t.Error("Should not enter here")
+	case <-time.NewTimer(1 * time.Second).C:
+	}
 }
